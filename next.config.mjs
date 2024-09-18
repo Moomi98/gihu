7d0bad2eb86219ec299179c7c34f57e6bb5b40dd
KimJeongHyeon <kymkjh2002@gmail.com>
2024-09-18 21:00:09 +0900

    axios 설치 및 proxy 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://localhost:8000/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
